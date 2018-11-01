8187fa37188b02189787ca2993f7e9985d1c1694
Justin Dennison <justin1dennison@gmail.com>
2018-10-31 20:09:27 -0400

    added the ability to change the box background color on click

--- src/App.js
+++ src/App.js
@@ -9,21 +9,30 @@ class App extends Component {
       selectedColor: '#ffff00'
     };
     this.onColorSelect = this.onColorSelect.bind(this);
+    this.changeBackground = this.changeBackground.bind(this);
   }
   onColorSelect(event) {
-    console.log(this)
     this.setState({
       selectedColor: event.target.value
     });
   }
+
+  changeBackground(event) {
+    this.setState({
+      boxColor: this.state.selectedColor
+    });
+  }
   render() {
+    const boxStyles = {
+      backgroundColor: this.state.boxColor
+    }
     return (
       <div className="App-container">
         <header className="App-header">
           <h1 className="title">Color Box</h1>
         </header>
         <main className="App-main">
-          <div className="box"></div>
+          <div className="box" onClick={this.changeBackground} style={boxStyles}></div>
           <input type="color" 
             value={this.state.selectedColor} 
             onChange={this.onColorSelect} 
